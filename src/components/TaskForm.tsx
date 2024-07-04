9156dedfbcaeaeee3a454723d3cbb1d7aa843dfe
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-04 16:02:19 -0300

    configuration

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const TaskForm = (props: Props) => {
+  return <div>TaskForm</div>
+}
+
+export default TaskForm
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,10 @@
+
+type Props = {}
+
+const TaskList = (props: Props) => {
+  return (
+    <div>TaskList</div>
+  )
+}
+
+export default TaskList
